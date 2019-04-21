60504cb76ce41deef2bc5e83ee7277c512392a1b
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 04:40:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(number)
   if number <= 1
+    (2..number-1).none? { |divide| number % divide == 0}
+  else
     return false
-  elsif number % 2 == 0
-    return false
-  else (2..number-1).none? { |divide| number % divide == 0}
   end
 end
